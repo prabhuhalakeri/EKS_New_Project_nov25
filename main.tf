17481b528e3845d324769cc0c717a9f8488c9961
halakerip <prabhu.halakeri@zohomail.com>
2025-11-17 20:55:24 +0530

    update2

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket         = "demo-terraform-eks-state-s3-bucket-krish123"
-    key            = "aws/terraform.tfstate"
+    key            = "aws/eks/terraform.tfstate"
     region         = "us-west-2"
    # dynamodb_table = "terraform-eks-state-locks"
     encrypt        = true
