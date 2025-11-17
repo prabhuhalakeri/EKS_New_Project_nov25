cc0188c0f2a7a4a9528b73a4c458a1bd10b2a6eb
halakerip <prabhu.halakeri@zohomail.com>
2025-11-17 20:52:41 +0530

    update1

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "demo-terraform-eks-state-s3-bucket-krish123"
     key            = "aws/terraform.tfstate"
     region         = "us-west-2"
-    dynamodb_table = "terraform-eks-state-locks"
+   # dynamodb_table = "terraform-eks-state-locks"
     encrypt        = true
   }
 }
